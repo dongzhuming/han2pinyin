00af4495f05726e76c673f2ce2bc6a6fd32c9864
xuender@gmail.com <xuender@gmail.com@7549d03c-1f1c-0410-8d67-b5ebee65c23b>
2011-04-09 13:00:20 +0000

    升级调2.0.1版本

---
+++ src/main/java/com/urlshow/pinyin/ToneType.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.urlshow.pinyin;
+
+/**
+ * 注音类型
+ * 
+ * @author ender
+ * 
+ */
+public enum ToneType {
+	/**
+	 * 声母
+	 */
+	SHENG_MU,
+	/**
+	 * 全拼
+	 */
+	QUAN_PIN,
+	/**
+	 * 词组
+	 */
+	CI_ZU,
+	/**
+	 * 语句
+	 */
+	YU_JU;
+}
